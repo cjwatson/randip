13d60f9fdc5c4342f95e0a936c25a97ee0916737
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-10-02 18:28:05 -0400

    Update randip.jl
    Rewrote script, added testing netmasks, randip still freezes on attempting to connect to a non-existent server(Something in socket)

--- randip.jl
+++ randip.jl
@@ -1,31 +1,60 @@
-function gen_ip()
-        ip = Any[]
-        for i in rand(1:255, 4)
-                push!(ip, i)
-        end
-        global ipaddr = join(ip, ".")
+##Network Mapper for Julia lang
+
+using IPNets
+
+##Static Functions
+function input(prompt::AbstractString = "")
+	print(prompt * " ")
+	chomp(readline())
 end
 
-function test_ip()
-        gen_ip()
-        println("Testing IP: ", ipaddr)
-        global socket = connect(ipaddr, 80)
+function genIP()
+  ip = Any[]
+  for i in rand(1:255, 4)
+    push!(ip, i)
+  end
+  global ranaddr = join(ip, ".")
 end
 
-function timer()
-        sleep(5)
-        close(socket)
+##Argument Functions
+function NetMask()
+  inputIP = input("Enter IP starting range: ")
+  mask = "/24"
+  ipAddr = IPv4Net(string(inputIP, mask))
+  i = 1
+  while i < length(ipAddr)
+    try
+      connect(ipAddr[i], 80)
+      print("Host: ", ipAddr[i], " up!..\n")
+      i = i + 1
+    catch LoadError
+      print("Host: ", ipAddr[i], " down..\n")
+      i = i + 1
+    end
+  end
+end
+
+function randIP()
+  genIP()
+  k = 0
+  while k != 1
+    try
+      connect(ranaddr, 80)
+      print("Host: ", ranaddr, " up!..\n")
+    catch LoadError
+      print("Host: ", ranaddr, " down..\n")
+    end
+  end
 end
 
 try
-        while true
-                @async test_ip()
-                @async timer()
-                timer()
-        end
-catch err
-        println("Simple Error:\n", err, "\n")
-        println("Detailed Error: ", error(), "\n")
-        println("Bactrace: ", backtrace())
-        exit()
+  if ARGS[1] == "-n"
+    NetMask()
+  elseif ARGS[1] == "-r"
+    randIP()
+  else
+    print("Usage: -n Netmask -r randip\n")
+  end
+catch BoundsError
+  print("Usage: -n Netmask -r randip\n")
 end
