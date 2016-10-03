e2153e621ea1e2bd8500734b3dcc9fed5ab8431c
Jacob Doyelle Yates <blmvxer@gmail.com>
2016-10-03 19:29:36 -0400

    randip still "broken"
    So here we are, Julia's socket doesn't have a programmable timeout, which for us means 30+ seconds of stalling, which has lead me to dive into parallel computing to mitigate this issue while hopefully bringing a powerful new feature to RandIP(Julia Version)

--- randip.jl
+++ randip.jl
@@ -25,7 +25,7 @@ end
 function NetMask()
   inputIP = input("Enter IP starting range: ")
   mask = "/24"
-  ipAddr = IPv4Net(string(inputIP, mask))
+  ipAddr = IPv4Net("$inputIP$mask")
   i = 1
   while i < length(ipAddr)
     try
@@ -48,45 +48,24 @@ function randIP()
       #socket0
       print("Connecting to ", ranaddr, " on socket0\n")
       socket0 = @async connect(ranaddr, 80)
-      task0 = current_task()
       #socket1
       genIP()
       print("Connecting to ", ranaddr, " on socket1\n")
       socket1 = @async connect(ranaddr, 80)
-      task1 = current_task()
       #socket2
       genIP()
       print("Connecting to ", ranaddr, " on socket2\n")
       socket2 = @async connect(ranaddr, 80)
-      task2 = current_task()
       #socket3
       genIP()
       print("Connecting to ", ranaddr, " on socket3\n")
       socket3 = @async connect(ranaddr, 80)
-      task3 = current_task()
       sleep(10)
       print("Host: ", ranaddr, " up!..\n")
       genIP()
-    catch
-      if istaskdone(task0) == true
-        global mytask = "task0"
-        tasks()
-        genIP()
-      elseif istaskdone(task1) == true
-        global mytask = "task1"
-        tasks()
-        genIP()
-      elseif istaskdone(task2) == true
-        global mytask = "task2"
-        tasks()
-        genIP()
-      elseif istaskdone(task3) == true
-        global mytask = "task3"
-        tasks()
-        genIP()
-      else
-        continue
-      end
+    catch LoadError
+      tasks()
+      genIP()
     end
   end
 end
