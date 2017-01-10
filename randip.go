1bd300e566ccdd78e8ecb7301cf99e17c54e7b32
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-01-10 12:26:36 -0500

    Create randip.go

---
+++ randip.go
@@ -0,0 +1,50 @@
+//Testing port fot Golang
+
+package main
+
+import (
+        "fmt"
+        "math/rand"
+        "time"
+        "strings"
+        "net"
+)
+
+var IP = ""
+
+func aToS(a [4]int, delim string) string {
+        return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
+
+func genIP() {
+        var IPaddr [4]int
+        i := 0
+        for i != 4 {
+                s1 := rand.NewSource(time.Now().UnixNano())
+                r1 := rand.New(s1)
+                IPaddr[i] = r1.Intn(255)
+                i += 1
+        }
+IP = aToS(IPaddr, ".")
+}
+
+func main() {
+        x := 0
+        for x != 10 {
+                genIP()
+                fmt.Println("Attempting connection on " + IP)
+                IPconn := (IP + ":80")
+                timeout := time.Duration(5) * time.Second
+                conn, err := net.DialTimeout("tcp", IPconn , timeout)
+                if err != nil {
+                        fmt.Println(err)
+                        fmt.Println("")
+                        continue
+                }
+        defer conn.Close()
+        buffer := make([]byte, 1024)
+        conn.Read(buffer)
+        fmt.Println(IP + ": Connected")
+        x += 1
+        }
+}
