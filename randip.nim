de1c14a4631aa921e3050fb761199927c3977a5a
Jacob Doyelle Yates <blmvxer@gmail.com>
2017-02-26 16:43:30 -0500

    Update randip.nim

--- randip.nim
+++ randip.nim
@@ -1,23 +1,29 @@
-import random, strutils, httpclient
+import random, strutils, httpclient, libnotify
+
 
 proc connect(url: string, timeout: int): tuple[msg: string, ok: bool] =
   try:
+    let me  = newNotifyClient("RandIP")
     let res = httpclient.request(url,
                                  httpMethod = httpclient.HttpHEAD,
                                  timeout = timeout * 1000)
     if "200 OK" == res.status:
+      let fixIp = url & " 200 OK"
+      let cstr: string = fixIp
+      send_new_notification(me, "RandIP", cstr, "", timeout=0, urgency=NotificationUrgency.Normal)
       echo res.status
       return (res.status, true)
+    elif "401 Unauthorized" == res.status:
+      let fixIp = url & " 401 Unauthorized"
+      let cstr: string = fixIp
+      send_new_notification(me, "RandIP", cstr, "", timeout=0, urgency=NotificationUrgency.Normal)
+      echo res.status
     else:
       echo res.status
       return (res.status, false)
   except:
     echo getCurrentExceptionMsg()
     return ("Unknown error", false)
-
-
-
-
 
 while true:
    randomize()
