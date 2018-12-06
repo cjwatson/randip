3766dc5f59c544e2fc6969e37675b32526f39448
Jacob Doyelle Yates <blmvxer@gmail.com>
2018-12-06 11:58:59 -0500

    Update randip.py

--- randip.py
+++ randip.py
@@ -11,7 +11,6 @@ args = sys.argv[1:]
 for arg in args:
 	if arg == '-nim':
 		print("Using nim version")
-		os.system('chmod +x /snap/randip/current/snap/randip_nim')
 		subprocess.call("/snap/randip/current/snap/randip_nim")
 		exit()
 	else:
